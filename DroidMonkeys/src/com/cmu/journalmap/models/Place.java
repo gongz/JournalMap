50a5f96a232b134772e100d5663ba8e0472af042
Kathryn Thomas <kathryn.thomas@sv.cmu.edu>
2012-11-08 19:15:07 -0800

    Added Place object and exif function

---
+++ DroidMonkeys/src/com/cmu/journalmap/models/Place.java
@@ -0,0 +1,11 @@
+package com.cmu.journalmap.models;
+
+public class Place
+{
+	private String photoLocation;
+	private String note;
+	private String audioLocation;
+	private String videoLocation;
+
+	
+}
